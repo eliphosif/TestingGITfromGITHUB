37eba16e06fbfef26622761f4b7201dbe90757ca
Ch <shiva.kishore.ch@accenture.com>
2021-12-12 18:17:18 +0530

    Java test2 git commit

--- src/testGit/Main.java
+++ src/testGit/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First line kishore");
+		System.out.println("Second line kishore");
 	}
 
 }
